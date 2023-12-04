3ad6904fcf858a9e2e802dabb61731ae098c2857
Vũ Tiến Quốc <42103573+Kedoichi@users.noreply.github.com>
2023-12-04 16:34:37 +1100

    fix minnor bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Header from '@/components/Header'
+import Header from '../components/Header'
+import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 

36a3ed53181fa1186535c56db966fefa233ec635
TommyBui89 <126430838+TommyBui89@users.noreply.github.com>
2023-12-11 13:13:22 +1100

    change

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const MainHeader = () => (
-  <header className="p-5 flex justify-between items-center max-w-7xl mx-auto bg-white shadow-md">
+  <header className="p-5 flex justify-between items-center mx-auto bg-white shadow-md">
     <motion.div
       initial={{ x: -100, opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
@@ -13,18 +13,20 @@ const MainHeader = () => (
       <img src="Logo2.png" alt="Logo 2" style={{ width: '9vw' }} />
     </motion.div>
 
-    <nav className="flex space-x-4">
-      {['Home', 'Our Services', 'Our Projects', 'About Us', 'Contact Us'].map(text => (
+    <nav className="flex space-x-2 md:space-x-4 lg:space-x-6">
+      {['Home', 'Our Services', 'Our Projects', 'About Us', 'Contact Us'].map((text) => (
         <motion.p
           key={text}
           className="uppercase text-black cursor-pointer hover:text-blue-600"
-          style={{ fontSize: '1.3vw' }}
+          style={{ fontSize: '1.2vw' }} // Adjust this value as needed
           whileHover={{ scale: 1.1 }}
         >
           {text}
         </motion.p>
       ))}
     </nav>
+
+
   </header>
 );
 

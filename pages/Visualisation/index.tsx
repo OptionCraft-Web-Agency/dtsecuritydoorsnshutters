c6ef8cbb564e71e68dd22db095dd78608695074b
Vũ Tiến Quốc <42103573+Kedoichi@users.noreply.github.com>
2023-12-18 20:26:03 +1100

    add nav to vis

--- pages/Visualisation/index.tsx
+++ pages/Visualisation/index.tsx
@@ -10,6 +10,7 @@ import Image from "next/image";
 import CanvasComponent from "@/components/Canvas";
 import RollerComponent from "@/components/Roller";
 import CostCalc from "@/components/CostCalc";
+import MainHeader from "@/components/MainHeader";
 
 interface ColorSetters {
   [key: string]: Dispatch<SetStateAction<string>>;
@@ -391,188 +392,193 @@ export default function Home() {
   };
 
   return (
-    <div className="container mx-auto">
-      <div className="flex bg-red-200 flex-col-reverse lg:flex-row ">
-        <div className="w-full lg:w-1/2 2xl:w-1/3 wide:w-3/10">
-          <div className="flex flex-row-reverse justify-evenly max-h-full lg:flex-row">
-            {/* Color Selection */}
-            <div className="overflow-auto w-full">
-              <div className="mx-auto my-auto">
-                {!curtainColor && (
-                  <div className="grid grid-cols-2 gap-2 my-10 flex-1 md:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-4 ultraWide:grid-cols-5 wide:mb-20">
-                    {sections[selectedSection]?.map((color: Color) => (
-                      <div
-                        key={color.name}
-                        className="flex flex-col items-center"
-                      >
-                        <button
-                          className="w-24 h-24 rounded-md border border-gray-200 shadow-md cursor-pointer mb-2"
-                          style={{ backgroundColor: color.code }}
-                          onClick={() =>
-                            handleColorSelection(selectedSection, color)
-                          }
-                        ></button>
-                        <span className="text-sm text-gray-700  sm:text-lg md:text-base 2xl:text-lg             ">
-                          {color.name}
-                        </span>
-                      </div>
-                    ))}
-                  </div>
-                )}
-
-                {!curtainColor && (
-                  <div className="grid grid-cols-2 gap-2 my-10 flex-1 md:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-4 ultraWide:grid-cols-5 wide:mb-20">
-                    {roller[selectedSection]?.map((color: Color) => (
-                      <div
-                        key={color.name}
-                        className="flex flex-col items-center"
-                      >
-                        <button
-                          className="w-24 h-24 rounded-md border border-gray-200 shadow-md cursor-pointer mb-2"
-                          style={{ backgroundColor: color.code }}
-                          onClick={() =>
-                            handleRollerSelection(selectedSection, color)
-                          }
-                        ></button>
-                        <span className="text-sm text-gray-700  sm:text-lg md:text-base 2xl:text-lg             ">
-                          {color.name}
-                        </span>
-                      </div>
-                    ))}
-                  </div>
-                )}
-
-                {curtainColor && (
-                  <div className="grid grid-cols-2 gap-2 my-10 flex-1 md:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-4 ultraWide:grid-cols-5 wide:mb-20">
-                    {Curtain.map((color, index) => (
-                      <div key={index} className="flex flex-col items-center">
-                        <button
-                          className="w-24 h-24 rounded-md border border-gray-200 shadow-md cursor-pointer mb-2"
-                          style={{ backgroundColor: color.code }}
-                          onClick={() => {
-                            updateCurtainColor(selectedIndex, color.code);
-                          }}
-                        ></button>
-                        <span className="text-sm text-gray-700 sm:text-lg md:text-base 2xl:text-lg">
-                          {color.name}
-                        </span>
-                      </div>
-                    ))}
-
-                    {/* Dropdown to select curtain number */}
-                    <div className="col-span-2 md:col-span-3 2xl:col-span-3 3xl:col-span-4 ultraWide:col-span-5">
-                      <select
-                        className="w-full h-10 pl-3 pr-6 text-base placeholder-gray-600 border rounded-lg appearance-none focus:shadow-outline text-center"
-                        value={selectedIndex}
-                        onChange={handleIndexChange}
-                      >
-                        {Array.from({ length: 23 }, (_, i) => (
-                          <option key={i} value={i}>
-                            Curtain {i + 1}
-                          </option>
-                        ))}
-                      </select>
+    <>
+      <MainHeader />
+      <div className="container mx-auto">
+        <div className="flex bg-red-200 flex-col-reverse lg:flex-row ">
+          <div className="w-full lg:w-1/2 2xl:w-1/3 wide:w-3/10">
+            <div className="flex flex-row-reverse justify-evenly max-h-full lg:flex-row">
+              {/* Color Selection */}
+              <div className="overflow-auto w-full">
+                <div className="mx-auto my-auto">
+                  {!curtainColor && (
+                    <div className="grid grid-cols-2 gap-2 my-10 flex-1 md:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-4 ultraWide:grid-cols-5 wide:mb-20">
+                      {sections[selectedSection]?.map((color: Color) => (
+                        <div
+                          key={color.name}
+                          className="flex flex-col items-center"
+                        >
+                          <button
+                            className="w-24 h-24 rounded-md border border-gray-200 shadow-md cursor-pointer mb-2"
+                            style={{ backgroundColor: color.code }}
+                            onClick={() =>
+                              handleColorSelection(selectedSection, color)
+                            }
+                          ></button>
+                          <span className="text-sm text-gray-700  sm:text-lg md:text-base 2xl:text-lg             ">
+                            {color.name}
+                          </span>
+                        </div>
+                      ))}
                     </div>
-                  </div>
-                )}
+                  )}
 
-                {/* TODO:: Adding custom color input */}
+                  {!curtainColor && (
+                    <div className="grid grid-cols-2 gap-2 my-10 flex-1 md:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-4 ultraWide:grid-cols-5 wide:mb-20">
+                      {roller[selectedSection]?.map((color: Color) => (
+                        <div
+                          key={color.name}
+                          className="flex flex-col items-center"
+                        >
+                          <button
+                            className="w-24 h-24 rounded-md border border-gray-200 shadow-md cursor-pointer mb-2"
+                            style={{ backgroundColor: color.code }}
+                            onClick={() =>
+                              handleRollerSelection(selectedSection, color)
+                            }
+                          ></button>
+                          <span className="text-sm text-gray-700  sm:text-lg md:text-base 2xl:text-lg             ">
+                            {color.name}
+                          </span>
+                        </div>
+                      ))}
+                    </div>
+                  )}
+
+                  {curtainColor && (
+                    <div className="grid grid-cols-2 gap-2 my-10 flex-1 md:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-4 ultraWide:grid-cols-5 wide:mb-20">
+                      {Curtain.map((color, index) => (
+                        <div key={index} className="flex flex-col items-center">
+                          <button
+                            className="w-24 h-24 rounded-md border border-gray-200 shadow-md cursor-pointer mb-2"
+                            style={{ backgroundColor: color.code }}
+                            onClick={() => {
+                              updateCurtainColor(selectedIndex, color.code);
+                            }}
+                          ></button>
+                          <span className="text-sm text-gray-700 sm:text-lg md:text-base 2xl:text-lg">
+                            {color.name}
+                          </span>
+                        </div>
+                      ))}
+
+                      {/* Dropdown to select curtain number */}
+                      <div className="col-span-2 md:col-span-3 2xl:col-span-3 3xl:col-span-4 ultraWide:col-span-5">
+                        <select
+                          className="w-full h-10 pl-3 pr-6 text-base placeholder-gray-600 border rounded-lg appearance-none focus:shadow-outline text-center"
+                          value={selectedIndex}
+                          onChange={handleIndexChange}
+                        >
+                          {Array.from({ length: 23 }, (_, i) => (
+                            <option key={i} value={i}>
+                              Curtain {i + 1}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
+                    </div>
+                  )}
+
+                  {/* TODO:: Adding custom color input */}
+                </div>
               </div>
-            </div>
-            <div className="flex flex-col my-10 xl:my-auto px-2">
-              {Object.keys(sections).map((section) => (
+              <div className="flex flex-col my-10 xl:my-auto px-2">
+                {Object.keys(sections).map((section) => (
+                  <button
+                    key={section}
+                    className={`px-4 py-2 m-1 ${
+                      selectedSection === section ? "bg-gray-200" : "bg-white"
+                    } border border-gray-300 rounded shadow`}
+                    onClick={() => {
+                      setSelectedSection(section);
+                      showCurtainColor(false);
+                    }}
+                  >
+                    {section}
+                  </button>
+                ))}
                 <button
-                  key={section}
                   className={`px-4 py-2 m-1 ${
-                    selectedSection === section ? "bg-gray-200" : "bg-white"
+                    showRoller ? "bg-green-200" : "bg-slate-300"
                   } border border-gray-300 rounded shadow`}
                   onClick={() => {
-                    setSelectedSection(section);
+                    setShowRoller(!showRoller);
                     showCurtainColor(false);
                   }}
                 >
-                  {section}
+                  Roller Shutter
                 </button>
-              ))}
-              <button
-                className={`px-4 py-2 m-1 ${
-                  showRoller ? "bg-green-200" : "bg-slate-300"
-                } border border-gray-300 rounded shadow`}
-                onClick={() => {
-                  setShowRoller(!showRoller);
-                  showCurtainColor(false);
-                }}
-              >
-                Roller Shutter
-              </button>
-              {showRoller && (
-                <div className="flex flex-col my-10 xl:my-auto">
-                  {Object.keys(roller).map((section) => (
-                    <button
-                      key={section}
-                      className={`px-4 py-2 m-1 ${
-                        selectedSection === section ? "bg-gray-200" : "bg-white"
-                      } border border-gray-300 rounded shadow`}
-                      onClick={() => {
-                        setSelectedSection(section);
-                        showCurtainColor(false);
-                      }}
-                    >
-                      {section}
-                    </button>
-                  ))}
-                </div>
-              )}
-              {showRoller && (
-                <button
-                  className={`px-4 py-2 m-1 ${
-                    curtainColor ? "bg-gray-200" : "bg-white"
-                  } border border-gray-300 rounded shadow`}
-                  onClick={() => {
-                    showCurtainColor(!curtainColor);
-                    setSelectedSection("");
-                  }}
-                >
-                  Curtains
-                </button>
-              )}
+                {showRoller && (
+                  <div className="flex flex-col my-10 xl:my-auto">
+                    {Object.keys(roller).map((section) => (
+                      <button
+                        key={section}
+                        className={`px-4 py-2 m-1 ${
+                          selectedSection === section
+                            ? "bg-gray-200"
+                            : "bg-white"
+                        } border border-gray-300 rounded shadow`}
+                        onClick={() => {
+                          setSelectedSection(section);
+                          showCurtainColor(false);
+                        }}
+                      >
+                        {section}
+                      </button>
+                    ))}
+                  </div>
+                )}
+                {showRoller && (
+                  <button
+                    className={`px-4 py-2 m-1 ${
+                      curtainColor ? "bg-gray-200" : "bg-white"
+                    } border border-gray-300 rounded shadow`}
+                    onClick={() => {
+                      showCurtainColor(!curtainColor);
+                      setSelectedSection("");
+                    }}
+                  >
+                    Curtains
+                  </button>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="w-full  lg:max-h-[1000px] lg:w-1/2 2xl:w-2/3 wide:w-7/10 mx-auto">
-          <div className="w-full h-[400px] md:h-[550px] xl:h-full">
-            <div
-              style={{ position: "relative", width: "100%", height: "100%" }}
-            >
-              <CanvasComponent
-                door={doorColor}
-                facia={facia}
-                frontWall={frontWall}
-                left={leftWallColor}
-                lowerRoof={lowerRoofColor}
-                pillars={pillarsColor}
-                right={rightWallColor}
-                roof={roofMainColor}
-                width={containerSize.width}
-                height={containerSize.height}
-              />
-
-              {showRoller && (
-                <RollerComponent
-                  curtainsColor={curtainsColor}
-                  bottom={bottom}
-                  rail={rail}
-                  headBox={headBox}
-                  slat={slat}
+          <div className="w-full  lg:max-h-[1000px] lg:w-1/2 2xl:w-2/3 wide:w-7/10 mx-auto">
+            <div className="w-full h-[400px] md:h-[550px] xl:h-full">
+              <div
+                style={{ position: "relative", width: "100%", height: "100%" }}
+              >
+                <CanvasComponent
+                  door={doorColor}
+                  facia={facia}
+                  frontWall={frontWall}
+                  left={leftWallColor}
+                  lowerRoof={lowerRoofColor}
+                  pillars={pillarsColor}
+                  right={rightWallColor}
+                  roof={roofMainColor}
                   width={containerSize.width}
                   height={containerSize.height}
                 />
-              )}
+
+                {showRoller && (
+                  <RollerComponent
+                    curtainsColor={curtainsColor}
+                    bottom={bottom}
+                    rail={rail}
+                    headBox={headBox}
+                    slat={slat}
+                    width={containerSize.width}
+                    height={containerSize.height}
+                  />
+                )}
+              </div>
             </div>
           </div>
         </div>
+        <CostCalc />
       </div>
-      <CostCalc />
-    </div>
+    </>
   );
 }

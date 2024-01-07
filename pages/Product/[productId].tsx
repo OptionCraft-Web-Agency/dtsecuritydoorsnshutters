2253b27dc4ba094b37d9e7c55e6d0632cbe2be47
Vũ Tiến Quốc <42103573+Kedoichi@users.noreply.github.com>
2024-01-08 00:26:37 +1100

    responsive

--- pages/Product/[productId].tsx
+++ pages/Product/[productId].tsx
@@ -4,6 +4,9 @@ import { useRouter } from "next/router";
 import { useQuery } from "@apollo/client";
 import { GET_PRODUCT_QUERY } from "@/graphql/getProduct";
 import ProductDetails from "@/components/ProductDetails";
+import Header from "@/components/Header";
+import MainHeader from "@/components/MainHeader";
+import Footer from "@/components/Footer";
 
 const ProductPage = () => {
   const router = useRouter();
@@ -18,7 +21,15 @@ const ProductPage = () => {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div>{data?.product && <ProductDetails product={data.product} />}</div>
+    <>
+      <Header />
+      <div className="bg-[#FFFCF8] w-screen wide:w-[80%] ultraWide:w-[75%] mx-auto">
+        <MainHeader />
+        <div>{data?.product && <ProductDetails product={data.product} />}</div>
+
+        <Footer />
+      </div>
+    </>
   );
 };
 

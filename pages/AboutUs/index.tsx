0a48a75ad381ba754ceb847adc62ac75aaccba42
TommyBui89 <126430838+TommyBui89@users.noreply.github.com>
2024-01-14 12:43:41 +1100

    fix

--- pages/AboutUs/index.tsx
+++ pages/AboutUs/index.tsx
@@ -4,12 +4,11 @@ import React, { CSSProperties } from 'react';
 
 import Header from '@/components/Header';
 import MainHeader from '@/components/MainHeader';
-import ContactUsInfo from '@/components/ContactUsInfo';
-import ContactUsForm from '@/components/ContactUsForm';
+import AboutUsInfo from '@/components/AboutUsInfo';
 import Footer from '@/components/Footer';
 
 // Define the AboutUsSection component
-const ContactUsSection: React.FC = () => {
+const AboutUsSection: React.FC = () => {
   const sectionStyle: CSSProperties = {
     position: 'relative',
     width: '100%',
@@ -18,7 +17,7 @@ const ContactUsSection: React.FC = () => {
     justifyContent: 'center',
     alignItems: 'center',
     color: 'white',
-    fontSize: 'min(4vw, 7vw)', // Adjusted for better text scaling
+    fontSize: 'min(4vw, 7vw)',
     fontWeight: 'bold',
     textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
     background: `linear-gradient(180deg, rgba(136, 136, 138, 0.54) 0%, rgba(0, 87, 255, 0.29) 100%), url('/RollerDoor3.png') center/cover no-repeat`,
@@ -26,19 +25,20 @@ const ContactUsSection: React.FC = () => {
 
   return (
     <div style={sectionStyle}>
-      Contact Us
+      About Us
     </div>
   );
 };
 
-export default function ContactUs() {
+export default function AboutUs() {
   return (
     <div>
       <Header />
       <MainHeader />
-      <ContactUsSection />
-      <ContactUsInfo />
-      <ContactUsForm />
+      <AboutUsSection />
+      <div style={{margin:'80px 0px'}}>
+      <AboutUsInfo />
+      </div>
       <Footer />
     </div>
   );

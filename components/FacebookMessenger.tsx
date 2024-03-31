6b36fff854df04df33843b1666cf370e7e2a4868
TommyBui89 <126430838+TommyBui89@users.noreply.github.com>
2024-04-01 00:54:13 +1100

    change to enlgish

--- components/FacebookMessenger.tsx
+++ components/FacebookMessenger.tsx
@@ -1,10 +1,9 @@
-"Use Client";
-import React, { Component } from "react";
+import React from "react";
 import { FacebookProvider, CustomChat } from "react-facebook";
 
 const FacebookMessenger = () => {
   return (
-    <FacebookProvider appId="1642943909801365" chatSupport>
+    <FacebookProvider appId="1642943909801365" chatSupport language="en_US">
       <CustomChat
         pageId="202813109587848"
         minimized={false ? true : undefined}

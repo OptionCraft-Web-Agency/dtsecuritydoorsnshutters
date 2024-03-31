1719d37db6d62b337099ad5a13e7cc93f4d8c402
Vũ Tiến Quốc <42103573+Kedoichi@users.noreply.github.com>
2024-03-31 22:39:17 +0700

    disconnect from old domain

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "admin.quocchic.net",
-        // You can also add pathname to be more specific, or leave it out to allow all paths
-      },
-    ],
-  },
+  
   i18n: {
     locales: ['en', 'fr', 'es'], // List of locales your application supports
     defaultLocale: 'en', // The default locale used when visiting a non-locale prefixed path

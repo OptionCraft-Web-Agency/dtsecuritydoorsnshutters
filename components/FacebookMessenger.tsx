f668b43adcb3457647087c6b8ba21d63e6c32949
Vũ Tiến Quốc <42103573+Kedoichi@users.noreply.github.com>
2024-03-31 22:38:54 +0700

    Hotfix

--- components/FacebookMessenger.tsx
+++ components/FacebookMessenger.tsx
@@ -3,7 +3,7 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 
 const FacebookMessenger = () => {
   return (
-    <FacebookProvider appId="1642943909801365" chatSupport language="en_US">
+    <FacebookProvider appId="1095362448401059" chatSupport language="en_US">
       <CustomChat
         pageId="202813109587848"
         minimized={false ? true : undefined}
--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
       },
     ],
   },
+  i18n: {
+    locales: ['en', 'fr', 'es'], // List of locales your application supports
+    defaultLocale: 'en', // The default locale used when visiting a non-locale prefixed path
+  },
 };
 
 module.exports = nextConfig;
